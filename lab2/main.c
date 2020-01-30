e26a55cfc77da8b3d3014c5073d8e25f0fb950bd
John Hodson <hodsonus@gmail.com>
2020-01-30 10:41:41 -0500

    lab2 init

---
+++ lab2/main.c
@@ -0,0 +1,15 @@
+#include <msp.h>
+#include <driverlib.h>
+#include <stdio.h>
+
+/* ---------------------------------------- MAIN ---------------------------------------- */
+
+/**
+ * main.c
+ */
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
+
+    PCM_gotoLPM0(); // enter LPM mode
+}
